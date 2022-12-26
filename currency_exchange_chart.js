7c7b172737e10f0f1683a4280a784f84a7c0e28b
ikorotkaya <irinkakorotkaya@gmail.com>
2022-12-26 16:24:14 +0100

    Initial commit

---
+++ currency_exchange_chart.js
@@ -0,0 +1,41 @@
+const ctx = document.getElementById('myChart');
+
+const labels = [1, 2, 3, 4, 5, 6, 50]
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1, 2, 3, 4, 5, 6, 100],
+      borderColor: "red",
+    }
+  ]
+};
+
+
+new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Currency Exchange'
+      }
+    },
+    scales: {
+      x: {
+        type: 'linear',
+        title: {
+          display: true,
+          text: 'Date'
+        }
+      }
+    },
+  },
+});
+
